c66b8f6a94f6ae9cc945421b3b21e022b8798aec
ssp13 <ssp13@users.noreply.github.com>
2014-08-09 12:22:35 +0300

    Update charsRemaining.js

--- charsRemaining.js
+++ charsRemaining.js
@@ -13,39 +13,42 @@ app.directive('charsRemaining',function($compile){
   return{
     restrict:'A',
     link:function(scope,elem,attrs){
-		var maxChars = scope.firstMaxChars;
-		var br=angular.element("</br>");
+	//get maxchars from controller
+	var maxChars = scope.firstMaxChars;
+	//or get maxchars directly from elem attribute
+	//var maxChars = Number(attrs.charRemaining);
+	var br=angular.element("</br>");
 	//using span.text()	  	
-		var span = angular.element('<span ></span>');
-	    elem.after(br);
-	    br.after(span);
+	var span = angular.element('<span ></span>');
+  	elem.after(br);
+        br.after(span);
 	//using {{}}
-	    var markup =$compile( "<span>using interpolation: {{remaining}} </span> ")(scope);
-	    span.after(br);
-	    br.after(markup);
-        console.log(elem);
-            scope.$watch(function(){
-              var val=elem.val();
+	var markup =$compile( "<span>using interpolation: {{remaining}} </span> ")(scope);
+	span.after(br);
+	br.after(markup);
+            
+        scope.$watch(function(){
+	    var val=elem.val();
               return val;
             },function(newValue,oldValue){
               console.log(newValue,oldValue);
-              if (newValue){
+              	if (newValue){
                  var remaining=maxChars-newValue.length;
                  
                      if(remaining>0){
-						             span.text(remaining);
-						            scope.remaining=remaining;
+			span.text(remaining);
+			scope.remaining=remaining;
                         
                  	 }else{
                     	span.text("exceeded")
-					  	scope.remaining="exceeded";
-                     }
+			scope.remaining="exceeded";
+                         }
               }else {
               scope.remaining=maxChars;
               span.text(maxChars);
             }
            }
-          )
-       }
-  	}
- })
+          );
+        }
+    };
+ });
